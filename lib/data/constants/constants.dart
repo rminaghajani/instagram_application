cd119baf8d8586002506948834d96bf467507b12
Armin Aghajani <rminaghajani@gmail.com>
2023-08-01 20:37:08 +0330

    add colors to project

---
+++ lib/data/constants/constants.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+const Color mainColor = Color(0xff1C1F2E);
+const Color pink = Color(0xffF35383);
+const Color gray = Color(0xffC5C5C5);
+const Color white = Color(0xffffffff);
+const Color blue = Color(0xff4E3CC9);
