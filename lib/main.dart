fe6e0cf47432441d30b01414823ad9140e02dbd0
Armin Aghajani <rminaghajani@gmail.com>
2023-08-06 13:11:29 +0330

    theme of widgets added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:instagram/screens/splash_screen.dart';
+import 'package:instagram/screens/switch_screen.dart';
+
+import 'data/constants/constants.dart';
 
 void main() {
   runApp(Application());
@@ -11,8 +14,32 @@ class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: pink,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            textStyle: TextStyle(
+              color: white,
+              fontFamily: 'GB',
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+        textTheme: TextTheme(
+          headlineMedium: TextStyle(
+            color: white,
+            fontFamily: 'GB',
+            fontSize: 15,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: SwitchScreen(),
     );
   }
 }
